fcc0f45422f641d9791c22b7238c6c5edd0d2b8e
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-22 03:38:20 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,13 @@ require 'pry'
 def my_find(collection)
   i =0
   while i < collection.length
-  i += 1
+  if yield(collection[i])
+    return collection[i]
   end
-
+  i += 1
+ end
 end
 
-def my_find(collection)
+collection = (1..100).to_a
+
+def my_find(collection) {|i| i % 3 == 0 && i %5 == 0}
